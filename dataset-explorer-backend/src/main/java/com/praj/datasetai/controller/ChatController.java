d9d6566d0839b1f6fcc74b8b17ea38887469a39d
Prajwalkadam29 <prajwal.kadam2005@gmail.com>
2026-01-03 01:32:46 +0530

    Updated the Chat Controller, the controller now returns a structured object

--- dataset-explorer-backend/src/main/java/com/praj/datasetai/controller/ChatController.java
+++ dataset-explorer-backend/src/main/java/com/praj/datasetai/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.praj.datasetai.controller;
 
+import com.praj.datasetai.dto.ChatResponse;
 import com.praj.datasetai.langchain.agent.DataAnalystAgent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,8 @@ public class ChatController {
     private final DataAnalystAgent agent;
 
     @PostMapping
-    public String ask(@RequestBody String question) {
+    public ChatResponse ask(@RequestBody String question) {
+        // AI now returns a full object instead of just a string
         return agent.chat(question);
     }
 }
