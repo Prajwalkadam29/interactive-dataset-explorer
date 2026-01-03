e49f46fc3ecdadb43087f5e482926aa690f0f823
Prajwalkadam29 <prajwal.kadam2005@gmail.com>
2026-01-03 13:52:33 +0530

    Updated ChatController.java for Memory Support

--- dataset-explorer-backend/src/main/java/com/praj/datasetai/controller/ChatController.java
+++ dataset-explorer-backend/src/main/java/com/praj/datasetai/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.praj.datasetai.controller;
 
+import com.praj.datasetai.dto.ChatRequest;
 import com.praj.datasetai.dto.ChatResponse;
 import com.praj.datasetai.langchain.agent.DataAnalystAgent;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,8 @@ public class ChatController {
     private final DataAnalystAgent agent;
 
     @PostMapping
-    public ChatResponse ask(@RequestBody String question) {
-        // AI now returns a full object instead of just a string
-        return agent.chat(question);
+    public ChatResponse ask(@RequestBody ChatRequest request) {
+        // request.getSessionId() will be used by LangChain4j as the @MemoryId
+        return agent.chat(request.getSessionId(), request.getQuestion());
     }
 }
